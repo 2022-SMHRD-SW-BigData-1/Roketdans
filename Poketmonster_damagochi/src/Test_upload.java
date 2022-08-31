002ab3e6c2796dcaab11fd430d73bfdd40c1297e
노강우 <112359985+NOHKANGWOO@users.noreply.github.com>
2022-08-31 11:16:15 +0900

    노강우 두번째 커밋

--- Poketmonster_damagochi/src/Test_upload.java
+++ Poketmonster_damagochi/src/Test_upload.java
@@ -3,5 +3,7 @@ public class Test_upload {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요 ^^");
 		System.out.println("팀 첫 번째 커밋!!");
+		
+		System.out.println("노강우-두번째 커밋");
 	}
 }
