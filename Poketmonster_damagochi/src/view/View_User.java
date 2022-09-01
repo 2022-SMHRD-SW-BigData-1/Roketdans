687ecb17294fed83c074da106d0808b734883304
smhrd <smhrd@DESKTOP-U7OJTGU>
2022-09-01 17:24:57 +0900

    메인화면

--- Poketmonster_damagochi/src/view/View_User.java
+++ Poketmonster_damagochi/src/view/View_User.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.util.Scanner;
 
 import controller.Controller_DAO;
@@ -14,11 +15,11 @@ public class View_User {
 		Scanner sc = new Scanner(System.in);
 
 		Controller_DAO dao = new Controller_DAO();
-		//유저 가입, 로그인에 따른 포켓몬 생성
+		// 유저 가입, 로그인에 따른 포켓몬 생성
 		User_Pokemon userPokemon = null;
 		// 유저 가입, 로그인에 따른 정보 초기화 차이
 		User_VO userVO = null;
-		
+
 		while (true) {
 			System.out.print("[1]회원가입  [2]로그인>>");
 			int menu = sc.nextInt();
@@ -31,7 +32,7 @@ public class View_User {
 				String pw = sc.next();
 				System.out.println("nick : ");
 				String nick = sc.next();
-				
+
 				int cnt = dao.join(id, pw, nick);
 				if (cnt > 0) {
 					System.out.println("등록 성공");
@@ -40,6 +41,7 @@ public class View_User {
 				}
 				userPokemon = new User_Pokemon(id);
 				userVO = new User_VO(id, pw, nick);
+				break;
 			} else if (menu == 2) {
 				// 로그인 기능 연결
 				System.out.println("ID : ");
@@ -52,8 +54,9 @@ public class View_User {
 				} else {
 					System.out.println("로그인 실패");
 				}
-				userPokemon = new User_Pokemon(id,pw);
+				userPokemon = new User_Pokemon(id, pw);
 				userVO = new User_VO(id, pw);
+				break;
 			}
 		}
 		// 뷰 유저 수정.
@@ -61,7 +64,7 @@ public class View_User {
 //=========================================================================================================	
 //	로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 
 //=========================================================================================================	
-		
+		System.out.println("-------------------------------");
+		System.out.println("[1]사냥터 [2]체육관도전 [3]상태창 확인 [4]치료센처 [5]세이브 [6]종료");
 	}
 }
-
