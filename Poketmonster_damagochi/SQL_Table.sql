a53eb1ff0bd03bbcd22d6b21c4b943e4bf08d3cd
smhrd <smhrd@DESKTOP-JAT45PE>
2022-09-01 16:41:14 +0900

    // 복합 pk

--- Poketmonster_damagochi/SQL_Table.sql
+++ Poketmonster_damagochi/SQL_Table.sql
@@ -14,8 +14,10 @@ hp number(10) not null,
 level_ number(10) default 1,
 exp number(10) default 0,
 type varchar2(20),
-constraint Poke_name_pk primary key(poke_name),
-constraint Poke_type check(type in ('물타입','불타입','풀타입')));
+id varchar2(20),
+constraint Poke_name_pk primary key(poke_name,id),
+constraint Poke_type_ck check(type in ('물타입','불타입','풀타입')),
+constraint Poke_id_fk foregin key (id) references join(id));
 
 insert into join values('test','testing','tester','');
 
