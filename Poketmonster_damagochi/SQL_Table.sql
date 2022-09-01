517f6ecf4178d801b3c440fa51333daa43609e48
smhrd <smhrd@DESKTOP-JAT45PE>
2022-09-01 14:50:16 +0900

    // sql 테이블 최종 수정

--- Poketmonster_damagochi/SQL_Table.sql
+++ Poketmonster_damagochi/SQL_Table.sql
@@ -10,10 +10,12 @@ constraint join_id_pk primary key(id));
 create table Poke (
 poke_name varchar2(20),
 skill_name varchar2(20),
-hp number(10),
+hp number(10) not null,
 level_ number(10) default 1,
 exp number(10) default 0,
 type varchar2(20),
 constraint Poke_name_pk primary key(poke_name),
 constraint Poke_type check(type in ('물타입','불타입','풀타입')));
 
+drop table poke;
+select * from poke;
