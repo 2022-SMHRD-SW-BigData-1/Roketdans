a9ebbc775e8ca643bc44e1b2bdee8b1958cd92ab
smhrd <smhrd@DESKTOP-5SATLC9>
2022-09-01 16:44:31 +0900

    뷰 유저 수정.

--- Poketmonster_damagochi/src/view/View_User.java
+++ Poketmonster_damagochi/src/view/View_User.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 import controller.Controller_DAO;
 import model.User_Pokemon;
+import model.User_VO;
 
 public class View_User {
 
@@ -13,8 +14,10 @@ public class View_User {
 		Scanner sc = new Scanner(System.in);
 
 		Controller_DAO dao = new Controller_DAO();
-		
+		//유저 가입, 로그인에 따른 포켓몬 생성
 		User_Pokemon userPokemon = null;
+		// 유저 가입, 로그인에 따른 정보 초기화 차이
+		User_VO userVO = null;
 		
 		while (true) {
 			System.out.print("[1]회원가입  [2]로그인>>");
@@ -36,7 +39,7 @@ public class View_User {
 					System.out.println("등록 실패");
 				}
 				userPokemon = new User_Pokemon(id);
-				
+				userVO = new User_VO(id, pw, nick);
 			} else if (menu == 2) {
 				// 로그인 기능 연결
 				System.out.println("ID : ");
@@ -49,13 +52,15 @@ public class View_User {
 				} else {
 					System.out.println("로그인 실패");
 				}
-
+				userPokemon = new User_Pokemon(id,pw);
+				userVO = new User_VO(id, pw);
 			}
 		}
+		// 뷰 유저 수정.
 //=========================================================================================================	
 //	로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 
 //=========================================================================================================	
-
+		
 	}
 }
 
