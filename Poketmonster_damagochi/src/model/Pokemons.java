e4569b338f6ad64410e8868b04ef147e39a9f83e
smhrd <smhrd@DESKTOP-5SATLC9>
2022-09-01 15:17:54 +0900

    eclips 테이블명 수정

--- Poketmonster_damagochi/src/model/Pokemons.java
+++ Poketmonster_damagochi/src/model/Pokemons.java
@@ -5,15 +5,20 @@ import java.util.Random;
 public abstract class Pokemons {
 	
 	Random rd = new Random();
+	// 레벨
 	int level;
+	// 고정 시작 레벨, 한계 레벨
 	final int max_level = 10; 
 	final int min_level = 1;
+	// 레벨 비례 경험치
 	int exp; 
 	int max_exp = (int)(level * 1.5);
 	int min_exp = 0;
-	int hp= level * 10 + (rd.nextInt(5)+1) ;
+	// 레벨 비례 포켓몬 체력
+	int hp;
 	int max_hp = (int)(hp * 1.5);
 	int min_hp = 0;
+	// 레벨 비례 기본 공격력, 스킬 공격력
 	int basic_attack = level*2;
 	int skill_attack = basic_attack * 2 + (rd.nextInt(5)+1);
 	String[] pokemons_names = {"꼬부기", "파이리", "이상해씨","개구마르","물짱이","야돈","포니타","브케인","불꽃숭이","치코리타","나무지기","모부기"};
--- Poketmonster_damagochi/src/model/User_Pokemon.java
+++ Poketmonster_damagochi/src/model/User_Pokemon.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 public class User_Pokemon extends Pokemons{
 	// 포켓몬 테이블에 정보가 null이라면 레벨 1
 	// 있다면 포켓몬 정보 끌어 초기화
@@ -7,6 +9,12 @@ public class User_Pokemon extends Pokemons{
 	
 	// 회원가입 할 경우 레벨 1로	
 	public User_Pokemon(String user_id) {
+		Random rd = new Random();
+		hp = level * 10 + (rd.nextInt(5)+1);
+	}
+	
+	// 로그인 했을 경우
+	public User_Pokemon() {
 		
 	}
 	
