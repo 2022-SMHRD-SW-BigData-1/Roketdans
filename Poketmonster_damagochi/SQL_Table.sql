69f6e4992c2951edf1bb6828d3947c2ca416195e
smhrd <smhrd@DESKTOP-JAT45PE>
2022-09-01 14:28:43 +0900

    // 이정민 db 정리

---
+++ Poketmonster_damagochi/SQL_Table.sql
@@ -0,0 +1,20 @@
+
+create table join(
+id varchar2(20),
+pw varchar2(20) not null,
+nick varchar2(20) not null.
+join_date date default sysdate,
+constraint join_id_pk primary key(id));
+
+create table Poke (
+poke_name varchar2(20),
+skill_name varchar2(20),
+max_hp number(10) not null,
+hp number(10),
+level_ number(10) default 1,
+exp number(10) default 0,
+max_exp number(10) not null,
+type varchar2(20),
+constraint Poke_name_pk primary key(poke_name),
+constraint Poke_type check(type in ('물타입','불타입','풀타입')));
+
