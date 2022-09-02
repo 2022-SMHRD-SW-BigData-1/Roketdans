4faa653e2562d43380b0c54f003646ace0c62384
노강우 <112359985+NOHKANGWOO@users.noreply.github.com>
2022-09-02 14:38:03 +0900

    // 노강우 - 메인

--- Poketmonster_damagochi/src/model/story.java
+++ Poketmonster_damagochi/src/model/story.java
@@ -4,16 +4,31 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class story {
-	
-	// 글자 따닥따닥
+	// 글자 멈춤 메소드
 	
 	private static void pause() {
 		try { System.in.read();} catch(IOException e) {}
 	}
+	
+	// 글자 천천히 출력 메소드
+	
+	public static void slowPrint(String message, long millisPerChar) {
+
+		try {
+			for (int i = 0; i < message.length(); i++) {
+				System.out.print(message.charAt(i));
+
+				Thread.sleep(millisPerChar);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
 	// 메인 타이틀
 	
 	public static void title() {   
-		  System.out.println("\r\n" + 
+		System.out.print("\r\n" + 
 		  		"______  _____  _   __ _____ ___  ___ _____  _   _ \r\n" + 
 		  		"| ___ \\|  _  || | / /|  ___||  \\/  ||  _  || \\ | |\r\n" + 
 		  		"| |_/ /| | | || |/ / | |__  | .  . || | | ||  \\| |\r\n" + 
@@ -31,167 +46,125 @@ public class story {
 			Scanner sc = new Scanner(System.in);
 			System.out.print("Enter 키를 누르시면 스토리가 진행됩니다.");
 			pause();pause();
-			System.out.println();
-			System.out.print("포켓몬 세계에 온걸 환영한다.");
-			pause();pause();
-			System.out.print("나의 이름은 오박사");
-			pause();pause();
-			System.out.print("모두에게 포켓몬 박사라고 존경받고 있다.");
-			pause();pause();
-			System.out.println();
-			System.out.print("너의 꿈인 포켓몬 마스터가 되기 위해서는");
-			pause();pause();
-			System.out.print("각 마을에 있는 체육관에 도전하여");
-			pause();pause();
-			System.out.print("뱃지를 획득하여야 한다!!");
-			pause();pause();
-			System.out.println();
-			System.out.print("각 체육관에서 뱃지를 총 2개 획득하게 된다면");
-			pause();pause();
-			System.out.print("포켓몬 마스터가 될수 있다!");
-			pause();pause();
-			System.out.print("체육관에 도전하기 위해서는 파트너 포켓몬과 함께");
-			pause();pause();
-			System.out.print("야생 포켓몬을 사냥해 레벨을 올려야만 도전할수 있다");
-			pause();pause();
-			System.out.println();
-			System.out.print("포켓몬 마스터가 되기 위한 여정을 떠나겠느냐? (Y/N)");
-			pause();pause();
+			String[] openingText = {"포켓몬 세계에 온걸 환영한다.","\t","나의 이름은 오박사", "모두에게 포켓몬 박사라고 존경받고 있다.","\t", "너의 꿈인 포켓몬 마스터가 되기 위해서는", "각 마을에 있는 체육관에 도전하여", "뱃지를 획득하여야 한다!!","\t", 
+					"각 체육관에서 뱃지를 총 2개 획득하게 된다면", "포켓몬 마스터가 될수 있다!","\t", "체육관에 도전하기 위해서는 파트너 포켓몬과 함께","야생 포켓몬을 사냥해 레벨을 올려야지만 도전할수 있다!","\t", "포켓몬 마스터가 되기 위한 여정을 떠나겠느냐? (Y/N)"};
+			for(int i = 0; i<openingText.length; i++) {
+				slowPrint(openingText[i]+"\n", 10);
+			}
 			while(true) {
 			System.out.print("입력해주세요 : ");
 			String click = sc.next();
 			if (click.equals("y")) {
-				System.out.print("포켓몬 세계에 입장합니다!");
+				System.out.println();
+				slowPrint("포켓몬 세계에 입장합니다!",10);
+				System.out.println();
+				System.out.println();
 				break;
 			}else if(click.equals("n")){
-				System.out.println("거절은 거절하겠네");
-				System.out.print("포켓몬 세계에 입장합니다!");
+				slowPrint("거절은 거절하겠네!!!",10);
+				System.out.println();
+				System.out.println();
+				slowPrint("포켓몬 세계에 입장합니다!",10);
+				System.out.println();
+				System.out.println();
 				break;
 			}else {
-				System.out.println("잘못 입력하셨습니다.");
+				slowPrint("잘못 입력하셨습니다.",10);
+				System.out.println();
 			}
 		}
-			System.out.println();
-			System.out.println();
 		}
 		
 		// 포켓몬 선택 멘트
 		
 		public static void choose(){
 		Scanner sc = new Scanner(System.in);
-		System.out.print("오박사 : 포켓몬 마스터가 되기 위해서는");
-		pause();pause();
-		System.out.print("파트너 포켓몬을 선택해야 한다!");
-		pause();pause();
+		System.out.println("=========== 포켓몬 선택 ===========");
 		System.out.println();
-		System.out.print("모험을 시작하려면 함께 할 포켓몬이 있어야하지 않겠나?");
-		pause();pause();
-		System.out.print("내가 여기 3마리의 포켓몬을 준비했으니 골라보게!!");
-		pause();pause();
-		System.out.println("[1].꼬부기   [2].파이리   [3].이상해씨");
+		String[] chooseText = {"오박사 : 포켓몬 마스터가 되기 위해서는","파트너 포켓몬을 선택해야 한다!","\t","모험을 시작하려면 함께 할 포켓몬이 있어야하지 않겠나?","내가 여기 3마리의 포켓몬을 준비했으니 골라보게!!","\t"};
+		for(int i = 0; i<chooseText.length; i++) {
+			slowPrint(chooseText[i]+"\n", 10);
+		}
+		System.out.println("[1] 꼬부기   [2] 파이리   [3] 이상해씨");
+		System.out.println();
 		while(true) {
 			System.out.print("포켓몬을 선택하세요 : ");
 			int choice = sc.nextInt();
 			if(choice == 1) {
-				 System.out.println("꼬부기 : 꼬북 꼬북");
+				slowPrint("꼬부기 : 꼬북! 꼬북!",10);
+				// 이미지 삽입
 				 break;
 		}else if(choice == 2) {
-				  System.out.println("파이리 : 푸와오왘");
+			slowPrint("파이리 : 푸와오왘!!",10);
 				  break;
 		}else if(choice == 3) {
-				  System.out.println("이상해씨 : 이상! 이상!");
+			slowPrint("이상해씨 : 이상! 이상!",100);
 				  break;
 		}else{
-			System.out.println("다시 입력해주세요");
+			slowPrint("잘못 입력하셨습니다.",10);
+			System.out.println();
 			}  
 		}
 		System.out.println();
-		System.out.println("오박사 : 좋은 포켓몬을 선택했군! 그럼 바로 모험을 떠나게!");
-		pause();pause();
+		System.out.println();
+		
+		slowPrint("오박사 : 좋은 포켓몬을 선택했군! 그럼 바로 모험을 떠나게!",10);
+		System.out.println();
+		slowPrint("태초마을에 오신걸 환영합니다!!!",10);
+		System.out.println();
+		System.out.println();
+		}
+		
+		// 사냥터
+		
+		public static void field(){
+			slowPrint("야생 포켓몬이 출현합니다! 조심하세요!",10);
+			System.out.println();
 		}
 		// 첫번째 체육관 도전 멘트
 		
 		public static void gym1(){
-			System.out.print("강우 관장 : 후후훗! 나한테 도전하로 왔다고?? 혼내준다!");
-			pause();pause();
+			slowPrint("강우 관장 : 후후훗! 감히 나한테 도전하로 왔다고?? 혼내준다!",100);
+			System.out.println();
 		}
 
 		// 두번째 체육관 도전 멘트
 			
 		public static void gym2(){
-			System.out.print("정민 관장 : 훗! 가소롭군! 자비는 바라지마라!!");
-			pause();pause();
+			slowPrint("정민 관장 : 훗! 가소롭군! 내겐 자비란 없다!!",100);
+			System.out.println();
 		}
 		
 		// 로켓단 등장 멘트
 		
 		public static void roketdan(){
-			System.out.print("나 : 뭐야? 뱃지가 사라졌어...");
-			pause();pause();
-			System.out.print("뱃지도둑이 훔쳐 간거 같아 ㅠㅠ");
-			System.out.println();
-			pause();pause();
-			System.out.print("뱃지 도둑이라고 말하신다면");
-			pause();pause();
-			System.out.print("대답해 드리는게 인지상정");
-			pause();pause();
-			System.out.print("이 세계의 파괴를 막기 위해");
-			pause();pause();
-			System.out.print("이 세계의 평화를 지키기 위해");
-			pause();pause();
-			System.out.print("사랑과 진실 어둠을 뿌리고 다니는");
-			pause();pause();
-			System.out.print("포켓몬의 감초 귀염둥이 악당");
-			pause();pause();
-			System.out.print("나는 지은!");
-			pause();pause();
-			System.out.print("나는 민진!");
-			pause();pause();
-			System.out.print("나는 냐옹 이다옹~");
-			pause();pause();
-			System.out.println();
+			String[] roketdanText = {"나 : 뭐야? 뱃지가 사라졌어...","뱃지도둑이 훔쳐 간거 같아 ㅠㅠ","\t",
+					"로켓단 : 뱃지 도둑이라고 말하신다면","대답해 드리는게 인지상정","이 세계의 파괴를 막기 위해","이 세계의 평화를 지키기 위해","사랑과 진실 어둠을 뿌리고 다니는","포켓몬의 감초 귀염둥이 악당","나는 지은!!","나는 민진!!","나는 냐옹 이다옹~"};
+			for(int i = 0; i<roketdanText.length; i++) {
+				slowPrint(roketdanText[i]+"\n", 10);
+			}
 		}
-		
 		public static void ending(){
 			Scanner sc = new Scanner(System.in);
-			System.out.print("오박사 : 오래간만이군!");
-			pause();pause();
-			System.out.print("포켓몬 마스터가 되었다는 소문은 들었네");
-			pause();pause();
-			System.out.print("정말 축하하네!!!");
-			pause();pause();
-			System.out.print("힘들일은 없었나?");
-			System.out.println();
-			pause();pause();
-			System.out.print("나 : 너무 재밌었습니다! 로켓단만 만나지 않았다면 말이죠");
-			pause();pause();
-			System.out.print("오박사 : 뭐? 로켓단을 만났다고 그 악랄한 놈들");
-			pause();pause();
-			System.out.print("그래도 뱃지를 뺏기지 않아서 정말 다행이군!");
-			System.out.println();
-			pause();pause();
-			System.out.print("그래 이제 뭘 하고 싶은가?");
-			pause();pause();
-			System.out.print("나 : 아직 결정하지 못했어요..");
-			pause();pause();
-			System.out.print("오박사 : 다른 포켓몬과 다시 여행을 떠나는건 어떤가?");
-			pause();pause();
-			System.out.print("재시작 하시겠습니까? (Y/N)");
-			pause();pause();
+			String[] endingText = {"오박사 : 오래간만이군!","포켓몬 마스터가 되었다는 소문은 들었네","정말 축하하네!!!","힘들일은 없었나?","\t","나 : 너무 재밌었습니다! 로켓단만 만나지 않았다면 말이죠","\t","오박사 : 뭐? 로켓단을 만났다고 그 악랄한 놈들",
+					"그래도 뱃지를 뺏기지 않아서 정말 다행이군!", "그래 이제 뭘 하고 싶은가?", "\t","나 : 아직 결정하지 못했어요..", "\t","오박사 : 다른 포켓몬과 다시 여행을 떠나는건 어떤가?", "\t","재시작 하시겠습니까? (Y/N)"};
+			for(int i = 0; i<endingText.length; i++) {
+				slowPrint(endingText[i]+"\n", 10);
+			}
 			while(true) {
 				System.out.print("입력해주세요 : ");
 			String retry = sc.next();
 			if (retry.equals("y")) {
-				System.out.print("처음부터 다시 시작합니다.");
+				slowPrint("처음부터 다시 시작합니다.",100);
 				break;
 			}else if(retry.equals("n")) {
-				System.out.print("게임을 종료합니다.");
+				slowPrint("게임을 종료합니다.",100);
 				break;
 			}else {
-				System.out.println("잘못 입력하셨습니다.");
+				slowPrint("잘못 입력하셨습니다.",100);
 			}
 		}
 		}
+		}
 		
-	}
 
--- Poketmonster_damagochi/src/view/View_User.java
+++ Poketmonster_damagochi/src/view/View_User.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 import controller.Controller_DAO;
 import model.User_Pokemon;
 import model.User_VO;
+import model.story;
 
 public class View_User {
 
 	public static void main(String[] args) {
-//=========================================================================================================	
-//	로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 
-//=========================================================================================================	
+
 		Scanner sc = new Scanner(System.in);
 
 		Controller_DAO dao = new Controller_DAO();
@@ -19,96 +18,50 @@ public class View_User {
 		User_Pokemon userPokemon = null;
 		// 유저 가입, 로그인에 따른 정보 초기화 차이
 		User_VO userVO = null;
+		story s = new story();
+
+		// 메인 타이틀
+		s.title();
+
+		// 회원가입 , 로그인
 
 		while (true) {
-			System.out.print("[1]회원가입  [2]로그인>>");
+			System.out.print("[1]회원가입  [2]로그인 >> ");
 			int menu = sc.nextInt();
+			System.out.println();
 
 			if (menu == 1) {
+				s.opening();
 				System.out.println("==========등록==========");
-				System.out.println("ID : ");
+				System.out.println();
+				System.out.print("ID : ");
 				String id = sc.next();
-				System.out.println("PW : ");
+				System.out.print("PW : ");
 				String pw = sc.next();
-				System.out.println("nick : ");
+				System.out.print("nick : ");
 				String nick = sc.next();
 
 				int cnt = dao.join(id, pw, nick);
 				if (cnt > 0) {
 					System.out.println("등록 성공");
+					System.out.println("=======================");
+					System.out.println();
+					s.choose();
 				} else {
 					System.out.println("등록 실패");
+					System.out.println("=======================");
+					System.out.println();
 				}
-//				 유저 포켓몬 선택  흐름 int check_num == 선택 번호
-				System.out.println("------------포켓몬 선택하기------------");
-				System.out.print("[1]꼬부기");
-				System.out.println("\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠤⠐⠂⠀⠀⠀⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣔⠁⠀⠀⠀⠀⠀⠄⣀⢀⣤⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣌⠇⠀⠀⠀⠀⠀⣰⣇⣀⣷⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⢰⡿⠀⠀⠀⠀⠀⠀⣿⣿⣿⢻⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⣎⠁⠠⠀⣀⠀⠀⠀⠙⠛⠻⣾⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠻⣷⣶⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⢀⡠⠄⠒⢈⠟⠛⠿⠿⣿⣿⣿⣿⠿⠻⣿⣶⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⢀⣀⡠⠊⠁⠀⠀⢠⠇⠀⠀⠀⠀⠀⠘⣳⠃⠀⠀⠘⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⢈⣿⣟⣀⠀⠀⢠⠃⠀⠀⠉⠐⡖⠒⢒⠃⢀⠀⠀⢠⣿⣿⢿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠚⠻⠿⠿⠷⠶⠞⡀⠀⠀⠀⠀⡇⠀⡜⣘⣽⢇⣴⣿⣿⠋⢾⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⣀⡀⠀⢰⠀⠘⠚⠿⣿⣿⡿⠟⡇⠀⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⢰⡜⠀⠈⠉⢸⠐⠒⠒⠒⠺⣀⣀⡀⣇⠀⣿⣿⣿⠀⢀⠴⠂⠉⠉⠉⣷⣆⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⡰⠁⢣⠀⠀⠀⢸⠀⠀⠀⢀⣴⡾⠛⠉⠉⠲⣽⣿⡏⣠⣧⣤⣤⣤⣶⣿⣿⣿⣧⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⢠⠀⠀⠈⢳⣄⣀⡼⣄⡀⠀⠀⡞⠀⠀⠀⠀⠀⢹⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⢸⡀⣠⣤⣿⣿⣶⠤⣀⣉⡙⣻⡇⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⢠⡺⣻⣿⣿⣿⣿⠇⠀⠀⠀⠉⠉⠉⣄⣀⣀⣀⣠⣿⡿⠿⢿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠋⠹⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠚⠙⠣⠟⠛⠚⠀");
-				System.out.print("[2]파이리");
-				System.out.println(""
-						+ " ⠀⠀⠀⠀⠀⠀⠀⡠⠔⠂⠉⠐⠲⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⢠⠎⠀⠀⠀⠀⠀⠀⣸⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⣧⢰⣾⣧⠀⢠⣿⣷⡏⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⣧⡇⠉⠀⠀⠈⢩⣼⣷⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⢰⠿⠇⣶⠀⠀⠀⠈⠐⢿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⢸⣶⣤⣰⣶⣾⣗⣤⣴⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣦⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣛⣉⣩⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠌⣼⣿⡀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⣀⡠⢼⣿⣿⠿⣿⣿⣿⠇⠒⠤⣄⡀⠀⠀⠀⠀⠀⠀⣷⣿⣇⠳⡄⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⢀⢖⣶⣏⠁⠀⢸⠏⠀⠀⠈⠻⡀⢀⠀⡤⢀⡍⣩⣿⡧⠀⠀⠌⠋⣻⠋⣠⡆⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⢹⠻⠿⠶⠶⡞⠀⠀⠀⠀⠀⠘⡾⣦⣴⠿⠿⠿⠛⠉⠀⠀⠁⡀⠁⢠⣿⠁⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠘⠈⠋⢂⠀⠀⠀⠀⠀⠀⠀⠀⡸⡞⠉⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⢇⠀⠀⢣⠀⠀⠀⠀⠀⠀⣰⣧⠃⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠔⠁⢣⡀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠈⢱⣀⣀⣠⣤⣾⣿⡟⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⡌⢀⣤⣾⣷⣄⠀⠀⠀⠀⢸⠀⣠⠀⣰⣾⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⠿⠖⠂⠀⠬⢿⣿⣿⣿⣿⣿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⢀⣐⢓⠟⡿⠿⠟⠃⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠈⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠓⠽⠖⠙⠀");
-				System.out.print("[3]이상해씨");
-				System.out.println("⠀"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣦⢤⡄⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠄⠒⢒⣶⣶⣶⣶⣿⣿⢿⣿⣿⠁⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⡀⢀⡀⠀⠀⠀⢠⣾⣷⣶⣾⣿⣏⣉⣠⣾⣿⣿⢿⣿⣷⠀⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠰⡀⠐⠊⠉⢁⣈⣉⠉⠁⠋⠁⠀⢸⣿⣿⣿⣟⣥⣿⣿⣿⣧⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⢀⠏⠀⠀⢰⣾⣿⡿⠋⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀\r\n"
-						+ "⠀⠀⢀⡂⣦⡄⡾⠀⠉⡁⠀⠀⠀⡠⣤⣄⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀\r\n"
-						+ "⠀⠀⡸⢰⢹⠃⠀⠠⣾⡇⠀⠃⡜⢰⡏⢹⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀\r\n"
-						+ "⠀⠀⡃⠸⡎⠀⠀⠀⠀⠁⠀⠀⠀⠸⣷⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀\r\n"
-						+ "⠀⠀⠻⣶⣤⣀⡀⠠⠀⠀⣀⣀⣁⣀⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠙⡿⣿⣏⣉⣉⣉⣛⣿⣿⣿⣿⡿⠛⠉⠀⢹⣿⡟⣋⣼⣿⣧⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠇⣠⣽⣿⣿⣿⣿⣿⣿⣿⡟⣀⣶⡇⠀⣼⡟⢾⣿⣿⣿⣿⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⠟⠻⣿⣿⣿⣿⠀⠀⣰⣿⣷⣾⣿⣿⣿⣿⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⢱⢾⡿⡿⠏⠀⠀⠘⣛⢛⠃⣠⣼⠟⠀⠹⢿⣿⣿⣿⠏⠀⠀⠀\r\n"
-						+ "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠋⠋⠁⠀⠀⠀⠀⠉⠈");
-				
-				int check_num= sc.nextInt();
-				
-				
+				System.out.println();
 				userPokemon = new User_Pokemon();
 				userVO = new User_VO(id, pw, nick);
 				break;
+
 			} else if (menu == 2) {
 				// 로그인 기능 연결
-				System.out.println("ID : ");
+				System.out.print("ID : ");
 				String id = sc.next();
-				System.out.println("PW : ");
+				System.out.print("PW : ");
 				String pw = sc.next();
 				boolean res = dao.login(id, pw);
 				if (res == true) {
@@ -121,14 +74,52 @@ public class View_User {
 				break;
 			}
 		}
-		// 뷰 유저 수정.
-		// 2차 수정
-//=========================================================================================================	
-//	로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 로그인 
-//=========================================================================================================	
-		System.out.println("-------------------------------");
-		System.out.println("[1]사냥터 [2]체육관도전 [3]상태창 확인 [4]치료센처 [5]세이브 [6]종료");
-		//임시 상태창  check_pokemon(userPokemon){
-		
+		System.out.print("===================== < 선택해주세요 > =====================");
+		System.out.println();
+		System.out.println();
+		System.out.println("[1]사냥터 [2]체육관도전 [3]상태창 확인 [4]치료센터 [5]세이브 [6]종료");
+		System.out.print("입력해주세요 : ");
+		int move = sc.nextInt();
+		if (move == 1) {
+			System.out.println();
+			System.out.print("사냥터로 이동합니다.");
+			System.out.println();
+			s.field();
+		} else if (move == 2) {
+			while (true) {
+				System.out.println();
+				System.out.print("[1] 첫번째 체육관 [2] 두번째 체육관");
+				System.out.print("어떤 체육관에 도전하시겠습니까? : ");
+				int gymchoice = sc.nextInt();
+				if (gymchoice == 1) {
+					System.out.println();
+					s.gym1();
+					break;
+				} else if (gymchoice == 2) {
+					System.out.println();
+					s.gym2();
+					break;
+				} else {
+					System.out.println();
+					System.out.print("잘못 입력하셨습니다.");
+				}
+			}
+		} else if (move == 3) {
+			System.out.println();
+			System.out.print("상태창을 출력합니다.");
+		} else if (move == 4) {
+			System.out.println();
+			System.out.print("치료센터로 이동합니다.");
+		} else if (move == 5) {
+			System.out.println();
+			System.out.print("현재까지 플레이 내역을 저장합니다.");
+		} else if (move == 6) {
+			System.out.println();
+			System.out.print("게임을 종료합니다.");
+		} else {
+			System.out.println();
+			System.out.print("잘못 입력하셨습니다!!");
+		}
+
 	}
 }
