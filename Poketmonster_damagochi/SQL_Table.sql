efe6eca772a657f543d450bb812d32153b7522f1
smhrd <smhrd@DESKTOP-JAT45PE>
2022-09-04 01:09:04 +0900

    //

--- Poketmonster_damagochi/SQL_Table.sql
+++ Poketmonster_damagochi/SQL_Table.sql
@@ -8,16 +8,16 @@ constraint join_id_pk primary key(id));
 
 
 create table Poke (
-id varchar2(20),
 poke_name varchar2(20),
 skill_name varchar2(20),
-level_ number(10) default 1,
-type varchar2(20),
-max_hp number(10) not null,
 hp number(10) not null,
+level_ number(10) default 1,
 exp number(10) default 0,
-constraint Poke_type_ck check(type in ('물','불','물')),
-constraint Poke_id_fk foreign key (id) references join(id))
+type varchar2(20),
+id varchar2(20),
+constraint Poke_name_pk primary key(poke_name,id),
+constraint Poke_type_ck check(type in ('물타입','불타입','풀타입')),
+constraint Poke_id_fk foreign key (id) references join(id));
 
 
 
