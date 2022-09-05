ad389585d33944f342cfd969ac0aad6791727aa3
노강우 <112359985+NOHKANGWOO@users.noreply.github.com>
2022-09-05 14:46:57 +0900

    212

--- Poketmonster_damagochi/src/controller/Bgmplayer.java
+++ Poketmonster_damagochi/src/controller/Bgmplayer.java
@@ -13,7 +13,7 @@ public class Bgmplayer {
 		bgmlist.add(new Bgm("포켓몬발견","C://Users/smhrd/Desktop/Bgm/야생포켓몬발견.mp3"));
 		bgmlist.add(new Bgm("시작브금", "C://Users/smhrd/Desktop/Bgm/메인브금.mp3"));
 		bgmlist.add(new Bgm("타격", "C://Users/smhrd/Desktop/Bgm/hit.mp3"));
-		bgmlist.add(new Bgm("엔딩", "C://Users/smhrd/Desktop/Bgm/엔딩.mp3"));
+		bgmlist.add(new Bgm("엔딩노래", "C://Users/smhrd/Desktop/Bgm/엔딩노래.mp3"));
 		bgmlist.add(new Bgm("저장", "C://Users/smhrd/Desktop/Bgm/저장.mp3"));
 		bgmlist.add(new Bgm("진화", "C://Users/smhrd/Desktop/Bgm/진화.mp3"));
 		bgmlist.add(new Bgm("레벨업", "C://Users/smhrd/Desktop/Bgm/레벨업.mp3"));
